0d492a32e603df6db25b9304ccde890653bc9d9c
Harsh Gautam <hgautam208@gmail.com>
2021-09-25 01:13:11 +0530

    modified to use pack images and css files

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
