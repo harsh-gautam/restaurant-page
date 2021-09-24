83b6f4c33c0a7a7037576cc03300ccb04faec7f9
Harsh Gautam <hgautam208@gmail.com>
2021-09-24 21:40:05 +0530

    added a module to create navbar

---
+++ src/navbar.js
@@ -0,0 +1,27 @@
+const createNavbar = ({ title, navbarItems }) => {
+  const nav = document.createElement("nav");
+  nav.id = "navbar";
+
+  // Title
+  const homeTitle = document.createElement("div");
+  homeTitle.classList.add("home-title");
+  let h1 = document.createElement("h1");
+  h1.textContent = title;
+  homeTitle.appendChild(h1);
+
+  // Navbar items
+  const navItems = document.createElement("div");
+  navItems.classList.add("nav-items");
+  // create items
+  for (let item in navbarItems) {
+    let itemSpan = document.createElement("span");
+    itemSpan.classList.add("nav-item");
+    itemSpan.textContent = item;
+    navItems.appendChild(itemSpan);
+  }
+
+  nav.appendChild(homeTitle);
+  nav.appendChild(navItems);
+};
+
+export default createNavbar;
